1ae6c9d0f50fc091a49d1f57716631c2764d5b9a
vico65 <alvicojr65@gmail.com>
2025-06-04 21:45:45 +0700

    menyicil bagian quotes 1

---
+++ public/js/quotes.js
@@ -0,0 +1,67 @@
+export const quotes = [
+    {
+        author : "Henry Manampiring",
+        origin : "Filosofi Teras",
+        caption : "Ada sebagaian hal dalam hidup yang berada di bawah kendali kita, ada yang tidak di bawah kendali kita",
+    },
+    {
+        author : "Angela Duckworth",
+        origin : "GRIT",
+        caption : "Berbeda dengan orang dewasa, bayi dan balita tidak keberatan sama sekali untuk belajar dari kesalahan",
+    },
+    {
+        author : "Nietzche",
+        origin : "GRIT",
+        caption : "Apa yang tidak membunuhku, akan membuatku lebih kuat",
+    },
+    {
+        author : "Angela Duckworth",
+        origin : "GRIT",
+        caption : "Anda tidak boleh berhenti pada hari yang buruk",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Aku memang menangisi Narcissus, tapi aku tak pernah memperhatikan apakah benar dia elok. Aku menangis karena setiap kali dia berlutut di tepianku, aku bisa melihat pantulan keelokanku sendiri di kedalaman matanya.",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Aku memang menangisi Narcissus, tapi aku tak pernah memperhatikan apakah benar dia elok. Aku menangis karena setiap kali dia berlutut di tepianku, aku bisa melihat pantulan keelokanku sendiri di kedalaman matanya.",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Hampir saja ia mengeluhkan beratnya jaket itu, namun kemudian dia teringat, berkat jaket itulah dia bisa mengatasi dinginnya hawa subuh",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Hampir saja ia mengeluhkan beratnya jaket itu, namun kemudian dia teringat, berkat jaket itulah dia bisa mengatasi dinginnya hawa subuh",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Yang membuat hidup ini menarik adalah kemungkinan untuk mewujudkan impian menjadi kenyataan",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Saat engkau menginginkan sesuatu, seluruh jagat raya bersatu padu untuk membantumu meraihnya",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Kita takut kehilangan apa yang kita miliki, entah itu hidup kita, harta benda kita, ataupun tanah kita. Tapi rasa takut ini menguap begitu kita memahami bahwa kisah-kisah hidup kita dan sejarah dunia ini ditulis oleh tangan yang sama",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Meski kadang aku suka mengeluh, itu karena aku ini hati manusia, dan hati manusia memang seperti itu. Orang-orang takut mengejar impian-impian mereka yang paling berharga, sebab mereka merasa tidak layak mendapatkannya, atau tidak akan pernah bisa mewujudkannya.",
+    },
+    {
+        author : "Paul Coelho",
+        origin : "Sang Alkemis",
+        caption : "Katakan pada hatimu, rasa takut akan penderitaan justru lebih menyiksa daripada penderitaan itu sendiri. Dan tak ada hati yang menderita saat mengejar impian - impiannya, sebab setiap detik pencarian itu bisa diibaratkan pertemuan kembali dengan Tuhan dan keabadian",
+    },
+]
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-
+import {contacts} from './contacts.js';
 
 //membuat navbar fixed by sandhika
 window.onscroll = function() {
@@ -135,72 +135,6 @@ let getRandomCharacter = () => {
    return chars[Math.floor(Math.random() * chars.length)]
 }
 
-const contacts = [
-   {
-      name: "gmail",
-      icon: "gmail",
-      link: "mailto:alvicojr65@gmail.com",
-      bgColor : "bg-red-500",
-      textColor : "text-white",
-      desc : "Email profesional saya, hubungi untuk kolaborasi!"
-   },
-   {
-      name: "instagram 1",
-      icon: "instagram",
-      link: "https://www.instagram.com/vico65_/",
-      bgColor : "bg-linear-to-tr from-yellow-300 via-red-500 to-fuchsia-500",
-      textColor : "text-white",
-      desc : "Akun instagram formal saya, Follow!!"
-   },
-   {
-      name: "github",
-      icon: "github",
-      link: "https://github.com/vico65",
-      bgColor : "bg-dark",
-      textColor : "text-white",
-      desc : "Kunjungi repository projek saya disini, Follow!!"
-   },
-   {
-      name: "linkedin",
-      icon: "linkedin",
-      link: "https://www.linkedin.com/in/alvico65/",
-      bgColor : "bg-primary",
-      textColor : "text-white",
-      desc : "Mari membangun koneksi profesional bersama!"
-   },
-   {
-      name: "instagram 2",
-      icon: "instagram",
-      link: "https://www.instagram.com/bjismangk0/",
-      bgColor : "bg-linear-to-tr from-yellow-300 via-red-500 to-fuchsia-500",
-      textColor : "text-white",
-      desc : "Akun ig kedua, random post about movie or meme."
-   },
-   {
-      name : "facebook",
-      icon : "facebook",
-      link : "https://www.facebook.com/alvico.casilas/",
-      bgColor : "bg-blue-700",
-      textColor : "text-white",
-      desc : "Sosmed yang sering saya gunakan saat ini, Add!!"
-   },
-   {
-      name : "youtube",
-      icon : "youtube",
-      link : "https://www.youtube.com/@alvico2228",
-      bgColor : "bg-red-600",
-      textColor : "text-white",
-      desc : "Channel yt, namun jarang upload konten di sini"
-   },
-   {
-      name : "tiktok",
-      icon : "tiktok",
-      link : "https://www.tiktok.com/@vico65_ ",
-      bgColor : "bg-dark",
-      textColor : "text-white",
-      desc : "Sosmed yang sering saya gunakan saat ini, Follow!!"
-   }
-]
 
 
 // membuat animasi tombol kontak bergeser kebawah ketika tombol kontak lainnya di hover
